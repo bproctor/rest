522b485c3c7533c4ae0d70c4d2966107c44ad689
Brad Proctor <bproctor@tombras.com>
2012-06-18 13:14:50 -0400

    allow extra curl options

--- rest.php
+++ rest.php
@@ -1,9 +1,20 @@
 <?php
 
 /**
- * Very simple REST Client
+ * rest.php
  *
- * @author Brad Proctor <brad@bradleyproctor.com>
+ * A very simple REST client
+ *
+ * Copyright (c) 2012 Brad Proctor. (http://bradleyproctor.com)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Brad Proctor
+ * @copyright   Copyright (c) 2012 Brad Proctor
+ * @license     MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link        http://bradleyproctor.com/
+ * @version     0.1
  */
 class Rest
 {
@@ -12,8 +23,9 @@ class Rest
 	 * Perform the request
 	 *
 	 * @param array $args
-	 *		url => The url
-	 *		data => Optional data
+	 *		1 => The url
+	 *		2 => Optional data
+	 *		3 => Optional extra curl options
 	 *
 	 * @return string
 	 *		Returns the output from the remote server
@@ -24,16 +36,19 @@ class Rest
 			throw new Exception('Expecting at least 1 parameter');
 		}
 		$data = isset ($args[1]) ? $args[1] : array();
+		$options = isset ($args[2]) ? $args[2] : null;
 
 		// Don't attempt to run if curl isn't installed
 		if ( ! function_exists('curl_init')) {
 			throw new Exception('CURL not available');
 		}
 
+		// Build the request
 		$ch = curl_init($args[0] . ($name == 'get' ? http_build_query($data) : ''));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
+		$options and curl_setopt($ch, $options);
 		switch ($name) {
 			case 'delete':
 			case 'post':
@@ -47,6 +62,7 @@ class Rest
 				throw new Exception('Invalid method');
 		}
 
+		// Perform the request and return the results
 		$out = curl_exec($ch);
 		curl_close($ch);
 		return $out;
